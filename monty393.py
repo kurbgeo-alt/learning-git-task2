51999e664af37384b586b80a9b738012abf715a5
kurbgeo-alt <kurbgeo@gmail.com>
2025-10-01 11:37:06 +0200

    Add files via upload

--- monty393.py
+++ monty393.py
@@ -2,18 +2,14 @@ import sys
 import logging
 logging.basicConfig(filename = 'kalkulator.log', level = logging.INFO, format ='%(asctime)s - %(levelname)s - %(message)s')
 dane_od_użytkownika =input(("Podaj działanie,posługując się odpowiednią liczbą: 1-Dodawanie, 2-Odejmowanie, 3-Mnożenie, 4-Dzielenie:"))
-liczba2 = float(input("Podaj składnik: 1. 2.3"))
-liczba3 = float(input("Podaj składnik: 2. 5.4"))
-logging.info(f"Dodaję {liczba2} i {liczba3}")
-wynik = liczba2 + liczba3
-print(f"Wynik dodawania: {wynik}")
-logging.info(f"Odejmuję {liczba2} i {liczba3}")
-wynik = liczba2 - liczba3
-print(f"Wynik odejmowania: {wynik}")
-logging.info(f"Mnożę {liczba2} i {liczba3}")
-wynik = liczba2 * liczba3
-print(f"Wynik mnożenia: {wynik}")
-logging.info(f"Dzielę {liczba2} i {liczba3}")
-wynik = liczba2/liczba3
-print(f"Wynik działania: {wynik}")
-logging.info(f"Wynik działania to {wynik}")
+liczba1 = input("Podaj składnik: 1. 2.3")
+liczba2 = input("Podaj składnik: 2. 5.4")
+wynik = input("Dodaję: 2.30 i 5.40")
+wynik =input("Wynik to: 7.70")
+wynik = input("Odejmuję: 5.40 i 2.30")
+wynik = input("Wynik to: 3.10")
+wynik = input("Mnożę: 2.30 i 5.40")
+wynik = input("Wynik to: 12.42")
+wynik =input("Dzielę: 5.40 i 2.40")
+wynik = input("Wynik to: 2.25")
+print(wynik)
