c4cfad32712fa8be762a8cfa54d46c0062466d73
kurbgeo-alt <kurbgeo@gmail.com>
2025-10-22 08:00:05 +0200

    Add files via upload

--- monty.542.py
+++ monty.542.py
@@ -1,25 +1,40 @@
 from faker import Faker
-
+def fake_contacts(is_business,amount):
+       fake = Faker(locale = 'pl_PL')
+       first_name = fake.first_name()
+       last_name = fake.last_name()
+       phone__number = fake.phone_number()  
+       company_position = fake.job()
+       company_number = fake.company_number()    
+       
+       
 class BaseContact:
-    def __init__(self , name, phone, email):
-        self.name = name
-        self.phone = phone
-        self.email = email
+    def __init__(self , first_name, last_name, phone, email):
+      self.first_name = first_name
+      self.last_name = last_name
+      self.phone = phone
+      self.email = email
+        
 
-    
+    @property
     def label_length(self):
       return len(self.name)
+      
     
     
     
     
 
     def contact(self): 
-       print(f'Wybieram numer +48 {self.phone} i dzwonię do {self.name}') 
+       print(f'Wybieram numer  {self.phone} i dzwonię do {self.first_name}') 
 
 class BusinessContact(BaseContact):
-    def __init__(self, name, phone, email, company_position, company_name, company_number):
+    def __init__(self, first_name, last_name, phone, email, company_position, company_name, company_number):
        super().__init__(name, phone, email)
+       self.first_name = first_name
+       self.last_name = last_name
+       self.phone = phone
+       self.email = email
        self.company_position = company_position
        self.company_name = company_name
        self.company_number = company_number
@@ -29,16 +44,8 @@ class BusinessContact(BaseContact):
           
        
     def contact(self): 
-       print(f'Wybieram numer służbowy +48 {self.company_number} i dzwonię do {self.name}')
+       print(f'Wybieram numer służbowy  {self.company_number} i dzwonię do {self.name} z firmy {self.company_name}')
 
-    def fake_contacts(is_business,amount): 
-       fake = Faker(['th_TH', 'pl_PL'])
-       company_position = fake.job()
-       company_name = fake.company()
-       email = fake.email()
-       name = fake.name()
-       telephone_number = fake.number()[3:]
-       company_number = fake.number()[3:]
     
     list_of_contacts = []
     for i in range(amount):
@@ -46,17 +53,17 @@ class BusinessContact(BaseContact):
           card = BusinessContact(name, telephone_number, email, company_number, company_position, company_name)
         else:
           card = BaseContact(name, telephone_number, email)
-        lista_of_contacts.append(card)
-
-    return lista_of_contacts
-
+        list_of_contacts.aZppend(card)
+        return list_of_contact
+                
 
 
 
 n = 10
 
-fake_contacts(True, n) # biznesowe kontakty
-fake_contacts(False, n) # normalne kontakty  
+contacts = create_contacts(True,5)
+for c in contacts:
+  c.contact()
 
 
 name = "Jan Kowalski"
