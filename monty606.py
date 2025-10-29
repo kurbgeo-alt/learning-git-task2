e440fa18fa0eef73de1af0cba621b5eb333279fc
kurbgeo-alt <kurbgeo@gmail.com>
2025-10-29 07:56:41 +0100

    Add files via upload

---
+++ monty606.py
@@ -0,0 +1,45 @@
+import sqlite3
+from sqlite3 import Error
+
+def create_connection(db_file):
+   """ create a database connection to a SQLite database """
+   conn = None
+   try:
+       conn = sqlite3.connect(db_file)
+       print(f"Connected to {db_file}, sqlite version: {sqlite3.version}")
+   except Error as e:
+       print(e)
+   finally:
+       if conn:
+           conn.close()
+
+def create_connection_in_memory():
+   """ create a database connection to a SQLite database """
+   conn = None
+   try:
+       conn = sqlite3.connect(":memory:")
+       print(f"Connected, sqlite version: {sqlite3.version}")
+   except Error as e:
+       print(e)
+   finally:
+       if conn:
+           conn.close()
+
+if __name__ == '__main__':
+   create_connection(r"database.db")
+   create_connection_in_memory()
+def add_project(conn, project):
+   """
+   Create a new project into the projects table
+   :param conn:
+   :param project:
+   :return: project id
+   """
+   sql = '''INSERT INTO projects(nazwa, start_date, end_date)
+             VALUES(?,?,?)'''
+   cur = conn.cursor()
+   cur.execute(sql, project)
+   return cur.lastrowid  
+conn = create_connection("database.db")
+project = ("Powtórka z angielskiego", "2020-05-11 00:00:00", "2020-05-13 00:00:00")
+pr_id = add_project(conn, project)   
